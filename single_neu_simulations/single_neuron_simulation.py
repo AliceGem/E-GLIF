3e351479e946a1bb7c9e1d5c96f6b7923d646649
AliceGem <alice.geminiani@polimi.it>
2020-11-30 11:46:08 +0100

    Param Annalisa

--- single_neu_simulations/single_neuron_simulation.py
+++ single_neu_simulations/single_neuron_simulation.py
@@ -88,11 +88,13 @@ while(n_simulation < 10):
     #param_all = [3.3313, 0.3556, 351.3812, 0.6634, 168.2647, -56.6884 ] #  last nopt (lambda 1.5 e tau 1.0)
     param_all = [3.4178, 0.3265, 283.0757, 1.2063, 62.5273, 70.3318] # median
     #param_all = [2.0178, 0.2265, 13.0757, 1.2063, 32.5273, -30.0] # median with trial and error
-    # param_all = [4.3029, 0.4314, 496.0874, 0.0007, 102.9319, 55.1788]         # optim7 with min cost and all constr negative
-    #param_all = [4.3029, 0.4314, 96.0874, 0.0007, 102.9319, -10.1788]         # trial and error from optim7 with min cost and all constr negative
-
+    # param_all = [4.3029, 0.4314, 496.0874, 0.0007, 102.9319, 55.1788]         # optim1-run7 with min cost and all constr negative
+    #param_all = [4.3029, 0.4314, 96.0874, 0.0007, 102.9319, -10.1788]         # trial and error from optim1-run7 with min cost and all constr negative
+    param_all = [0.4000, 0.0920, 141.5567, 1.1414, 262.4390, 100.0000]      #median optim2
+    param_all = [0.623, 0.064184852, 315.456, -0.254, 4416.384, 0.0]              # Test fitting Annalisa
 
     param_CA1PC = {'t_ref': 2.15, 'C_m': 90.0, 'tau_m': 15.0, 'V_th': -48.0, 'V_reset': -55.0,'E_L': -65.0}
+    param_CA1PC = {'t_ref': 2.15, 'C_m': 189.79, 'tau_m': 15.58, 'V_th': 26.0, 'V_reset': -58.5,'E_L': -65.0}     # Test fitting Annalisa
 
     eglif_cond_alpha_multisyn = {
                     "t_ref": param_CA1PC['t_ref'],
@@ -101,8 +103,8 @@ while(n_simulation < 10):
                     "V_reset": param_CA1PC['V_reset'],
                     "E_L": param_CA1PC['E_L'],
                     "Vinit": param_CA1PC['E_L'],
-                    "lambda_0": 1.1,
-                    "tau_V": 0.7,
+                    "lambda_0": 1.0,
+                    "tau_V":0.0000000000001,
                     "tau_m": param_CA1PC['tau_m'],
                     "I_e": param_all[5],
                     "kadap": param_all[0],
@@ -145,7 +147,7 @@ while(n_simulation < 10):
                 "label": "spikes"})
 
     m = nest.Create("multimeter",
-                    params = {"interval": 1.0,
+                    params = {"interval": 0.1,
                             "record_from": ["V_m", "V_th", "I_dep", "I_adap", "I_gen"],
                             "withgid": True,
                             "to_file": True,
@@ -160,7 +162,7 @@ while(n_simulation < 10):
     # True.
 
     # Experiments linear adaptive neuron model:
-    current_amplitude = [0.0,100.0,0.0,200.0,0.0,300.0,0.0,-100.0,0.0]
+    current_amplitude = [0.0,200.0,0.0,300.0,0.0,400.0,0.0,-200.0,0.0]
 
     # Durate intervalli dell'intero protocollo
     durate = [10, 0.4, 1, 0.4, 1, 0.4, 1, 1, 1]
