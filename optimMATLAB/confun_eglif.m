a09db75cd78e662ee70357fca09ab78f5d02306f
AliceGem <alice.geminiani@polimi.it>
2020-12-10 09:10:32 +0100

    Print constr

--- optimMATLAB/confun_eglif.m
+++ optimMATLAB/confun_eglif.m
@@ -4,14 +4,13 @@ function [ c, ceq ] = confun_eglif( param, low, up, neu_ind, Iin,  Cm, tau_m, E_
     
 %CONFUN check the constraints (both linear and non linear)
 
-    
     syms k_adap k2 Ist A2 k1 A1 Ie t1 tss t
  
     % Oscillation parameters
     % Frequency
     delta = (-1/tau_m(neu_ind)-norma(param(2),low(2),up(2)))^2-4*(norma(param(2),low(2),up(2))/tau_m(neu_ind)+norma(param(1),low(1),up(1))/Cm(neu_ind));     % [1/ms^2]
     Vinh_act=double(subs(Sp(1),{Ist,k_adap,k2,Ie},{Iin,norma(param(1),low(1),up(1)),norma(param(2),low(2),up(2)),norma(param(6),low(6),up(6))}));
-    Sp_ton=double(subs(Sp(1),{Ist,k_adap,k2,Ie},{0,norma(param(1),low(1),up(1)),norma(param(2),low(2),up(2)),norma(param(6),low(6),up(6))}));
+    Sp_ton=double(subs(Sp(1),{Ist,k_adap,k2,Ie},{0,norma(param(1),low(1),up(1)),norma(param(2),low(2),up(2)),norma(param(6),low(6),up(6))}))
 
    % Nonlinear inequality constraints
   c = [
@@ -31,7 +30,7 @@ function [ c, ceq ] = confun_eglif( param, low, up, neu_ind, Iin,  Cm, tau_m, E_
     
 ];
 
-con = [con;c'];
+%con = [con;c'];
     % Nonlinear equality constraints
         %ceq = [param(2)+1/tau_m(neu_ind)];      % To have null damping
         ceq = [];
